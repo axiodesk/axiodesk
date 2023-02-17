5da7f347fce914cc8655ac59fb9a52d3c62b05bc
ahadnur <ahadnur44@gmail.com>
2023-02-17 16:23:31 +0600

    blog data format updated

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -42,17 +42,18 @@ const ptComponents = {
 
   block: {
     h1: ({ children }) => (
-      <h1 className="text-4xl font-bold py-10">{children}</h1>
+      <h1 className="text-4xl font-bold pt-10">{children}</h1>
     ),
     h2: ({ children }) => (
-      <h2 className="text-3xl font-bold py-10">{children}</h2>
+      <h2 className="text-3xl font-bold pt-8">{children}</h2>
     ),
     h3: ({ children }) => (
-      <h3 className="text-2xl font-bold py-10">{children}</h3>
+      <h3 className="text-2xl font-bold pt-6">{children}</h3>
     ),
     h4: ({ children }) => (
-      <h4 className="text-xl font-bold py-10">{children}</h4>
+      <h4 className="text-xl font-bold pt-4">{children}</h4>
     ),
+    normal: ({ children }) => <p className="pt-2">{children}</p>,
 
     blockquote: ({ children }) => (
       <blockquote className="border-l-[#7C0221] border-l-4 pl-5 py-5 my-5">
